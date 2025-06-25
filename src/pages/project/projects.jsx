5c09b7deedf2c28b56827a9caf1e7eea8d03967a
digiexpouae <tech.digiexpo@gmail.com>
2025-06-26 02:35:17 +0500

    kamdar

--- src/pages/project/projects.jsx
+++ src/pages/project/projects.jsx
@@ -8,7 +8,7 @@ const project = () => {
        <>
        <Header />
         <Section1 text={<>Our Portfolio</>} backgroundImage={'/assets/projectimages/project-1.jpg'} />
-          <Section4 className={'bg-no-repeat bg-[length:115%_135%] bg-[position:left_top_10%]'} backgroundImage={'/assets/projectimages/project-2.jpg'} titleNumbertext={'105'} text={<> Residences<br /> By Kamdar</>} btntext={<>Explore More</>} p={true} text1={<> JVC, Dubai</>} />
+          <Section4  overlay={true} className={'bg-no-repeat bg-[length:115%_135%] bg-[position:left_top_10%]'} backgroundImage={'/assets/projectimages/project-2.jpg'} titleNumbertext={'105'} text={<> Residences<br /> By Kamdar</>} btntext={<>Explore More</>} p={true} text1={<> JVC, Dubai</>} />
         <Footer />
     </>
   )
