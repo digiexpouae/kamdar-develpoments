8696f164c5488e306df8cf687d2c51769a3bc9b6
Ahmedgito <mamooabid1@gmail.com>
2025-06-27 23:21:33 +0500

    fix

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,11 +1,12 @@
 import '../styles/globals.css'
 import StickyIcon from '../common/stickyicon/stickyicon';
+import RootLayout from '../components/RootLayout';
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <RootLayout>
       <Component {...pageProps} />
       <StickyIcon />
-    </>
+    </RootLayout>
   );
 }
