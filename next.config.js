1f99b56a71300f2066249334dd568d76b14bc474
Ahmedgito <mamooabid1@gmail.com>
2025-07-31 18:29:43 +0500

    FIxed the sublinks

--- next.config.js
+++ next.config.js
@@ -1,6 +1,37 @@
 const nextConfig = {
   // config options here
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      // Project routes
+      {
+        source: '/projects',
+        destination: '/project',
+      },
+      {
+        source: '/projects/:path*',
+        destination: '/project/:path*',
+      },
+      // About routes
+      {
+        source: '/about-us',
+        destination: '/about',
+      },
+      {
+        source: '/about-us/:path*',
+        destination: '/about/:path*',
+      },
+      // Contact routes
+      {
+        source: '/contact-us',
+        destination: '/contact',
+      },
+      {
+        source: '/contact-us/:path*',
+        destination: '/contact/:path*',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
