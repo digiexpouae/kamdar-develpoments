de0ec088b459653b9dcca07c5d912f290e7bcdb9
Ahmedgito <mamooabid1@gmail.com>
2025-08-05 16:19:57 +0500

    optimzied logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import { Sling as Hamburger } from 'hamburger-react';
 import { motion, AnimatePresence } from 'framer-motion';
 import Link from 'next/link';
 import Image from 'next/image';
-import logo from '../../public/assets/logo.png';
+import logo from '../../public/assets/logo.webp';
 import { useRouter } from 'next/router';
 
 const navLinks = [
