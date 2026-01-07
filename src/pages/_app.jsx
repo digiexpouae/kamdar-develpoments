13b6bf130988ad018fac122fd30d9c97d2cb0556
ramsha1993 <rimshasaleem1993@gmail.com>
2026-01-07 14:57:16 +0500

    meta pixel

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,7 +3,7 @@ import StickyIcon from '../common/stickyicon/stickyicon';
 import RootLayout from '../components/RootLayout';
 import SEO from '../components/seo/SEO';
 import { SITE } from '../config/site';
-
+import { useEffect } from 'react';
 // Default SEO configuration
 const defaultSEO = {
   title: SITE.openGraph.title,
@@ -30,7 +30,7 @@ const App = ({ Component, pageProps }) => {
     ...defaultSEO,
     ...(pageProps.seo || {})
   };
-  
+
   // Add any additional structured data from page props
   if (pageProps.structuredData) {
     seoProps.structuredData = {
@@ -38,6 +38,25 @@ const App = ({ Component, pageProps }) => {
       ...pageProps.structuredData
     };
   }
+
+
+  useEffect(() => {
+    // Facebook Pixel
+    !function (f, b, e, v, n, t, s) {
+      if (f.fbq) return; n = f.fbq = function () {
+        n.callMethod ?
+          n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+      };
+      if (!f._fbq) f._fbq = n; n.push = n; n.loaded = !0; n.version = '2.0';
+      n.queue = []; t = b.createElement(e); t.async = !0;
+      t.src = v; s = b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t, s)
+    }(window, document, 'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+
+    fbq('init', '2307403746375376');
+    fbq('track', 'PageView');
+  }, []);
 
   return (
     <>
